ec7e7b7f10a9310fe4d1f4f2b70e83cc6df544e7
Zane Kissel <zekissel@gmail.com>
2024-04-08 21:20:02 -0400

    export csv, not txt

--- score.py
+++ score.py
@@ -4,94 +4,100 @@ import pandas as pd
 # participant ID
 id = 2000
 
-# open a new file (named 'results.txt'), in write mode, with variable name result_file
-with open('scoring_file.txt', 'w') as scoring_file:
-  # create a loop: execute indented code for all found participant IDs
-  while id <= 2999:
+# create a dataframe with predefined columns to store results
+results = pd.DataFrame(columns=['PID', 'CON_RT', 'INC_RT', 'CON_ACC', 'INC_ACC'])
 
-    # try to run the following indented code: in case there are any errors, don't crash, but jump to 'except' instead
-    try:
+# create a loop: execute indented code for all found participant IDs
+while id <= 2999:
 
-      # call predefined Pandas function to read CSV file 
-      # MUST be in Flanker_csv directory/folder and named as C2xxx_flanker.csv
-      # read dataframe into 'df' variable
-      df = pd.read_csv(f'Flanker_csv/C{id}_flanker.csv')
+  # try to run the following indented code: in case there are any errors, don't crash, but jump to 'except' instead
+  try:
 
-      # display to terminal the first 5 rows of data (display first x rows with .head(x))
-      print(df.head())
+    # call predefined Pandas function to read CSV file 
+    # MUST be in Flanker_csv directory/folder and named as C2xxx_flanker.csv
+    # read dataframe into 'df' variable
+    df = pd.read_csv(f'Flanker_csv/C{id}_flanker.csv')
 
-      # clean up data: get rid of unnecessary columns and save trimmed down dataset to a new variable 'flanker_scoring'
-      flanker_scoring = df.drop([
-        'PID', 
-        'RegistrationID', 
-        'AssessmentName', 
-        'InstrumentID', 
-        'InstrumentName', 
-        'ThetaStandardError', 
-        'Theta', 
-        'TScore', 
-        'TScoreStandardError', 
-        'DateCreated', 
-        'NumberFormatting'
-        ], axis=1) #axis=1 means index the COLUMNS, not rows
+    # display to terminal the first 5 rows of data (display first x rows with .head(x))
+    print(df.head())
 
-      # continue cleanup: only keep rows where column 'ItemID' contains 'FLANKER_FISH_TRIAL'
-      # save dataset to new variable 'flanker_trials'
-      flanker_trials = flanker_scoring[flanker_scoring['ItemID'].str.contains('FLANKER_FISH_TRIAL')]
-      # now dataset only contains rows with 'FLANKER_FISH_TRIAL' in column 'ItemID';
-      # we can now remove the text 'FLANKER_FISH_TRIAL' from this column, so that we're left with just a trial number (e.g. 1-30)
-      flanker_trials.loc[: ,'ItemID'] = flanker_trials['ItemID'].map(lambda x: x.strip('FLANKER_FISH_TRIAL'))
+    # clean up data: get rid of unnecessary columns and save trimmed down dataset to a new variable 'flanker_scoring'
+    flanker_scoring = df.drop([
+      'PID', 
+      'RegistrationID', 
+      'AssessmentName', 
+      'InstrumentID', 
+      'InstrumentName', 
+      'ThetaStandardError', 
+      'Theta', 
+      'TScore', 
+      'TScoreStandardError', 
+      'DateCreated', 
+      'NumberFormatting'
+      ], axis=1) #axis=1 means index the COLUMNS, not rows
 
-      print(id)
-      print(flanker_trials)
+    # continue cleanup: only keep rows where column 'ItemID' contains 'FLANKER_FISH_TRIAL'
+    # save dataset to new variable 'flanker_trials'
+    flanker_trials = flanker_scoring[flanker_scoring['ItemID'].str.contains('FLANKER_FISH_TRIAL')]
+    # now dataset only contains rows with 'FLANKER_FISH_TRIAL' in column 'ItemID';
+    # we can now remove the text 'FLANKER_FISH_TRIAL' from this column, so that we're left with just a trial number (e.g. 1-30)
+    flanker_trials.loc[: ,'ItemID'] = flanker_trials['ItemID'].map(lambda x: x.strip('FLANKER_FISH_TRIAL'))
 
-      # define list that contains incongruent trial numbers
-      inc_index = [3, 5, 7, 10, 13, 15, 17, 20, 24, 26]
-      # incongruent (INC) 10 Trials: 3, 5, 7, 10, 13, 15, 17, 20, 24, 26
-      # congruent (CON) 20 Trials: !INC (not incongruent trials)
+    print(id)
+    print(flanker_trials)
 
-      # trial numbers are considered 'strings' by the code right now (even though its just a digit)
-      # we want to convert them to 'integers' so they can be compatible with number operations
-      temp = pd.to_numeric(flanker_trials['ItemID'])
-      flanker_trials.loc[ : ,'ItemID'] = temp
+    # define list that contains incongruent trial numbers
+    inc_index = [3, 5, 7, 10, 13, 15, 17, 20, 24, 26]
+    # incongruent (INC) 10 Trials: 3, 5, 7, 10, 13, 15, 17, 20, 24, 26
+    # congruent (CON) 20 Trials: !INC (not incongruent trials)
 
-      # to filter for INCONGRUENT trials, this function is called to void rows that are CONGRUENT (we can then drop the voided rows)
-      def zero_if_con(x):
-        if x not in inc_index: return 0
-        else: return x
-      # same as above, but filters for CONGRUENT trials by voiding INCONGRUENT rows
-      def zero_if_inc(x):
-        if x not in inc_index: return x
-        else: return 0
+    # trial numbers are considered 'strings' by the code right now (even though its just a digit)
+    # we want to convert them to 'integers' so they can be compatible with number operations
+    temp = pd.to_numeric(flanker_trials['ItemID'])
+    flanker_trials.loc[ : ,'ItemID'] = temp
 
-      # here, we are using the above functions as described to create two new dataframes: CON and INC
-      incongruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_con) != 0]
-      congruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_inc) != 0]
+    # to filter for INCONGRUENT trials, this function is called to void rows that are CONGRUENT (we can then drop the voided rows)
+    def zero_if_con(x):
+      if x not in inc_index: return 0
+      else: return x
+    # same as above, but filters for CONGRUENT trials by voiding INCONGRUENT rows
+    def zero_if_inc(x):
+      if x not in inc_index: return x
+      else: return 0
 
-      # write to file, using file variable 'scoring_file' opened at beginning
-      scoring_file.write(f'PID: {id}\n')
+    # here, we are using the above functions as described to create two new dataframes: CON and INC
+    incongruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_con) != 0]
+    congruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_inc) != 0]
 
-      # find reaction time (RT) for both INC and CON trials
-      # format score to four decimal places
-      response_time_con = congruent['ResponseTime'].mean()
-      scoring_file.write('CON RT:{:10.4f}\n'.format(response_time_con))
+    # write to file, using file variable 'scoring_file' opened at beginning
+    pid = f'{id}'
 
-      response_time_inc = incongruent['ResponseTime'].mean()
-      scoring_file.write('INC RT:{:10.4f}\n'.format(response_time_inc))
+    # find reaction time (RT) for both INC and CON trials
+    # format score to four decimal places
+    response_time_con = congruent['ResponseTime'].mean()
+    con_rt = '{:10.4f}'.format(response_time_con)
 
-      # find accuracy (ACC) for both INC and CON trials
-      # format score to four decimal places
-      accuracy_con = congruent['Score'].sum() / len(congruent)
-      scoring_file.write('CON ACC:{:10.4f}\n'.format(accuracy_con))
+    response_time_inc = incongruent['ResponseTime'].mean()
+    inc_rt = '{:10.4f}'.format(response_time_inc)
 
-      accuracy_inc = incongruent['Score'].sum() / len(incongruent)
-      scoring_file.write('INC ACC:{:10.4f}\n\n'.format(accuracy_inc))
+    # find accuracy (ACC) for both INC and CON trials
+    # format score to four decimal places
+    accuracy_con = congruent['Score'].sum() / len(congruent)
+    con_acc = '{:10.4f}'.format(accuracy_con)
 
+    accuracy_inc = incongruent['Score'].sum() / len(incongruent)
+    inc_acc = '{:10.4f}'.format(accuracy_inc)
 
-    # only gets run whenever 'try' block fails (file name does not exist); go to next ID
-    except:
-      pass
-    
+    # append participant data to end of output dataframe
+    results.loc[len(results.index)] = [pid, con_rt, inc_rt, con_acc, inc_acc]
 
-    # necessary for 'while' loop to advance to next participant ID
-    id += 1
+  # only gets run whenever 'try' block fails (file name does not exist); go to next ID
+  except:
+    pass
+  
+
+  # necessary for 'while' loop to advance to next participant ID
+  id += 1
+
+# export compiled dataframe to a .csv file
+results.to_csv('scoring_file.csv', sep='\t')
