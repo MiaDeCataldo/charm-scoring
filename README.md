7b2e9b1bcf44be31f844f0b049f3dceecaee2514
Zane Kissel <zekissel@gmail.com>
2024-04-08 20:45:10 -0400

    share code

---
+++ README.md
@@ -0,0 +1,35 @@
+# CHARM FLANKER SCORING
+
+## First time setup:
+1. For the code to be able to run, your computer must have a Python interpreter (v3 or greater). Confirm this with ```python3 --version```
+2. In a terminal window, navigate to the project directory (use 'cd {folder-name}').
+
+![Change directory example](img/cd.png)
+- *Working directory is shown in green (~ is equivalent to /home/user)*
+- *'pwd' will Print Working Directory, and was only used for demonstration*
+---
+
+3. Create an environment in order to install Pandas (framework we use to manipulate data):
+```python3 -m venv .venv``` <-- '-m venv' sets 'mode' to 'venv' (virtual environment), and '.venv' is name of resulting folder
+4. Put the environment that was just created into context: ```source .venv/bin/activate```
+
+![Create virtual environment and set context](img/venv.png)
+---
+
+5. Install dependency using ```pip install pandas``` <-- pip is a Python package manager, and Pandas is a data manipulation framework for Python
+
+![Install PANDAS](img/pandas.png)
+---
+
+6. Execute Python script with ```python3 score.py```
+7. The outputted scores will be sent to 'scoring_file.txt' in the same directory. Use command 'cat' to display the contents of the file: ```cat scoring_file.txt``` (or view in Operating System specific text viewer)
+
+![Score output](img/scores.png)
+---
+
+8. To remove virtual environment from context, use command ```deactivate```
+
+## For future runs:
+1. Once you create the virtual environment and install Pandas to it, you can reuse this environment again the next time.
+2. If you already have the environment configured, navigate to the same project directory and use the command again: ```source .venv/bin/activate```.
+3. No need to install anything this time. When done executing code that requires Pandas, use command ```deactivate``` (regular Python code can be executed without this virtual environment; when Pandas is required we can use the environment to keep our dependencies organized)
---
+++ score.py
@@ -0,0 +1,97 @@
+# import framework (PANDAS) for manipulating data
+import pandas as pd
+
+# participant ID
+id = 2000
+
+# open a new file (named 'results.txt'), in write mode, with variable name result_file
+with open('scoring_file.txt', 'w') as scoring_file:
+  # create a loop: execute indented code for all found participant IDs
+  while id <= 2999:
+
+    # try to run the following indented code: in case there are any errors, don't crash, but jump to 'except' instead
+    try:
+
+      # call predefined Pandas function to read CSV file 
+      # MUST be in Flanker_csv directory/folder and named as C2xxx_flanker.csv
+      # read dataframe into 'df' variable
+      df = pd.read_csv(f'Flanker_csv/C{id}_flanker.csv')
+
+      # display to terminal the first 5 rows of data (display first x rows with .head(x))
+      print(df.head())
+
+      # clean up data: get rid of unnecessary columns and save trimmed down dataset to a new variable 'flanker_scoring'
+      flanker_scoring = df.drop([
+        'PID', 
+        'RegistrationID', 
+        'AssessmentName', 
+        'InstrumentID', 
+        'InstrumentName', 
+        'ThetaStandardError', 
+        'Theta', 
+        'TScore', 
+        'TScoreStandardError', 
+        'DateCreated', 
+        'NumberFormatting'
+        ], axis=1) #axis=1 means index the COLUMNS, not rows
+
+      # continue cleanup: only keep rows where column 'ItemID' contains 'FLANKER_FISH_TRIAL'
+      # save dataset to new variable 'flanker_trials'
+      flanker_trials = flanker_scoring[flanker_scoring['ItemID'].str.contains('FLANKER_FISH_TRIAL')]
+      # now dataset only contains rows with 'FLANKER_FISH_TRIAL' in column 'ItemID';
+      # we can now remove the text 'FLANKER_FISH_TRIAL' from this column, so that we're left with just a trial number (e.g. 1-30)
+      flanker_trials.loc[: ,'ItemID'] = flanker_trials['ItemID'].map(lambda x: x.strip('FLANKER_FISH_TRIAL'))
+
+      print(id)
+      print(flanker_trials)
+
+      # define list that contains incongruent trial numbers
+      inc_index = [3, 5, 7, 10, 13, 15, 17, 20, 24, 26]
+      # incongruent (INC) 10 Trials: 3, 5, 7, 10, 13, 15, 17, 20, 24, 26
+      # congruent (CON) 20 Trials: !INC (not incongruent trials)
+
+      # trial numbers are considered 'strings' by the code right now (even though its just a digit)
+      # we want to convert them to 'integers' so they can be compatible with number operations
+      temp = pd.to_numeric(flanker_trials['ItemID'])
+      flanker_trials.loc[ : ,'ItemID'] = temp
+
+      # to filter for INCONGRUENT trials, this function is called to void rows that are CONGRUENT (we can then drop the voided rows)
+      def zero_if_con(x):
+        if x not in inc_index: return 0
+        else: return x
+      # same as above, but filters for CONGRUENT trials by voiding INCONGRUENT rows
+      def zero_if_inc(x):
+        if x not in inc_index: return x
+        else: return 0
+
+      # here, we are using the above functions as described to create two new dataframes: CON and INC
+      incongruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_con) != 0]
+      congruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_inc) != 0]
+
+      # write to file, using file variable 'scoring_file' opened at beginning
+      scoring_file.write(f'PID: {id}\n')
+
+      # find reaction time (RT) for both INC and CON trials
+      # format score to four decimal places
+      response_time_con = congruent['ResponseTime'].mean()
+      scoring_file.write('CON RT:{:10.4f}\n'.format(response_time_con))
+
+      response_time_inc = incongruent['ResponseTime'].mean()
+      scoring_file.write('INC RT:{:10.4f}\n'.format(response_time_inc))
+
+      # find accuracy (ACC) for both INC and CON trials
+      # format score to four decimal places
+      accuracy_con = congruent['Score'].sum() / len(congruent)
+      scoring_file.write('CON ACC:{:10.4f}\n'.format(accuracy_con))
+
+      accuracy_inc = incongruent['Score'].sum() / len(incongruent)
+      scoring_file.write('INC ACC:{:10.4f}\n\n'.format(accuracy_inc))
+
+
+    # only gets run whenever 'try' block fails (file name does not exist); go to next ID
+    except:
+      pass
+    
+
+    # necessary for 'while' loop to advance to next participant ID
+    id += 1
