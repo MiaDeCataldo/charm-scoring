233adf502c71f0a7a2368abf0e370183786c1d7d
Zane Kissel <zekissel@gmail.com>
2024-04-16 19:25:43 -0400

    edit flanker scoring to use glob; comment nback scoring

--- score-flanker.py
+++ score-flanker.py
@@ -1,55 +1,38 @@
 # import framework (PANDAS) for manipulating data: https://pandas.pydata.org/
 import pandas as pd
+import glob
 
-# participant ID
-id = 2000
+
+# read all matching file names in ./NBack_csv folder
+# this variable is a list of strings, all which are present and in form 'Flanker_csv/C2xxx_flanker*.csv'
+files = glob.glob('Flanker_csv/C2???_flanker*.csv')
 
 # create a dataframe with predefined columns to store results
 results = pd.DataFrame(columns=['PID', 'CON_RT', 'INC_RT', 'CON_ACC', 'INC_ACC'])
 
-# create a loop: execute indented code for all found participant IDs
-while id <= 2999:
+# iterate through files that glob found
+for f in files:
+    
+    print(f)
+    # read data into dataframe from local file
+    df = pd.read_csv(f)
 
-  # try to run the following indented code: in case there are any errors, don't crash, but jump to 'except' instead
-  try:
+    # names currently include directory; split string at '/' and take second half
+    filename = f.split('/')[1]
+    # 1. create list of characters from filename
+    # 2. access list from the 1st index to the 4th index to find participant ID (skips C for filenames like C2xxx)
+    # 3. resulting list is only integers that make up PID. Glue them together with empty string ('') to make one integer
+    pid = ''.join(list(filename)[1:5])
 
-    # call predefined Pandas function to read CSV file 
-    # MUST be in Flanker_csv directory/folder and named as C2xxx_flanker.csv
-    # read dataframe into 'df' variable
-    df = pd.read_csv(f'Flanker_csv/C{id}_flanker.csv')
-
-    # display to terminal the first 5 rows of data (display first x rows with .head(x))
-    print(df.head())
-
-    # clean up data: get rid of unnecessary columns and save trimmed down dataset to a new variable 'flanker_scoring'
-    flanker_scoring = df.drop([
-      'PID', 
-      'RegistrationID', 
-      'AssessmentName', 
-      'InstrumentID', 
-      'InstrumentName', 
-      'ThetaStandardError',
-      'Theta', 
-      'TScore', 
-      'TScoreStandardError', 
-      'DateCreated', 
-      'NumberFormatting'
-      ], axis=1) #axis=1 means index the COLUMNS, not rows
-
-    # continue cleanup: only keep rows where column 'ItemID' contains 'FLANKER_FISH_TRIAL'
+    # only keep rows where column 'ItemID' contains 'FLANKER_FISH_TRIAL'
     # save dataset to new variable 'flanker_trials'
-    flanker_trials = flanker_scoring[flanker_scoring['ItemID'].str.contains('FLANKER_FISH_TRIAL')]
+    flanker_trials = df[df['ItemID'].str.contains('FLANKER_FISH_TRIAL')]
     # now dataset only contains rows with 'FLANKER_FISH_TRIAL' in column 'ItemID';
     # we can now remove the text 'FLANKER_FISH_TRIAL' from this column, so that we're left with just a trial number (e.g. 1-30)
     flanker_trials.loc[: ,'ItemID'] = flanker_trials['ItemID'].map(lambda x: x.strip('FLANKER_FISH_TRIAL'))
 
-    print(id)
-    print(flanker_trials)
-
     # define list that contains incongruent trial numbers
     inc_index = [3, 5, 7, 10, 13, 15, 17, 20, 24, 26]
-    # incongruent (INC) 10 Trials: 3, 5, 7, 10, 13, 15, 17, 20, 24, 26
-    # congruent (CON) 20 Trials: !INC (not incongruent trials)
 
     # trial numbers are considered 'strings' by the code right now (even though its just a digit)
     # we want to convert them to 'integers' so they can be compatible with number operations
@@ -69,9 +52,6 @@ while id <= 2999:
     incongruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_con) != 0]
     congruent = flanker_trials[flanker_trials['ItemID'].apply(zero_if_inc) != 0]
 
-    # create participant ID variable to add to result dataframe
-    pid = f'{id}'
-
     # find reaction time (RT) for both INC and CON trials
     # format score to four decimal places
     response_time_con = congruent['ResponseTime'].mean()
@@ -90,12 +70,8 @@ while id <= 2999:
 
     # append participant data to end of output dataframe
     results.loc[len(results.index)] = [pid, con_rt, inc_rt, con_acc, inc_acc]
+    
 
-  # only gets run whenever 'try' block fails (file name does not exist); go to next ID
-  except: pass
-
-  # necessary for 'while' loop to advance to next participant ID
-  id += 1
-
-# export compiled dataframe to a .csv file
+results = results.sort_values(by='PID')
+# export output dataframe to local CSV file
 results.to_csv('flanker_scores.csv', sep=',', index=False)
--- score-nback.py
+++ score-nback.py
@@ -1,47 +1,54 @@
+# import framework (PANDAS) for manipulating data: https://pandas.pydata.org/
 import pandas as pd
 import glob
 
+
 # read all matching file names in ./NBack_csv folder
+# this variable is a list of strings, all which are present and in form 'NBack_csv/C2xxx_DualNBack_Task_*.csv'
 files = glob.glob('NBack_csv/C2???_DualNBack_Task_*.csv')
 
-results = pd.DataFrame(columns=['PID', '1B_AVG_CORR_RT', '1B_AVG_INCORR_RT', '1B_ACC', '2B_AVG_CORR_RT', '2B_AVG_INCORR_RT', '2B_ACC',])
+# create dataframe to store the output data
+results = pd.DataFrame(columns=['PID', '1B_AVG_CORR_RT', '1B_AVG_INCORR_RT', '1B_ACC', '2B_AVG_CORR_RT', '2B_AVG_INCORR_RT', '2B_ACC'])
 
+# iterate through each file found in subdirectory
 for f in files:
     
-    df = pd.read_csv(f)
-    df.dropna()
-
     print(f)
+    # read data into dataframe from local file
+    df = pd.read_csv(f)
 
+    # names currently include directory; split string at '/' and take second half
     filename = f.split('/')[1]
+    # 1. create list of characters from filename
+    # 2. access list from the 1st index to the 4th index to find participant ID (skips C for filenames like C2xxx)
+    # 3. resulting list is only integers that make up PID. Glue them together with empty string ('') to make one integer
     pid = ''.join(list(filename)[1:5])
 
+    # define list of column names that we want to access
+    columns = ['oneback_avg_corr', 'oneback_avg_incorr', 'oneback_corr', 'twoback_avg_corr', 'twoback_avg_incorr', 'twoback_corr']
+    # 1. access the raw dataframe at the columns specified above
+    # 2. from this view, drop every row that contains ALL null values (only 1 row should contain floats (decimals); all others blank)
+    # 3. save new view of dataframe to 'relavant' variable
+    relavant = df[columns].dropna(how='all')
+    
+    # iterate over rows in dataframe (this should only be one row)
+    for row in relavant.iterrows():
 
-    oneback_avg_corr_rt = df['oneback_avg_corr'].dropna()
-    if (len(oneback_avg_corr_rt.index) < 1): oneback_avg_corr_rt = None
-    else: oneback_avg_corr_rt = oneback_avg_corr_rt.iloc[0]
+        # access values in row (row[0] is index)
+        row = row[1]
+        # access columns in row
+        oneback_avg_corr_rt = row.iloc[0]
+        oneback_avg_incorr_rt = row.iloc[1]
+        oneback_acc = row.iloc[2]
 
-    oneback_avg_incorr_rt = df['oneback_avg_incorr'].dropna()
-    if (len(oneback_avg_incorr_rt.index) < 1): oneback_avg_incorr_rt = None
-    else: oneback_avg_incorr_rt = oneback_avg_incorr_rt.iloc[0]
+        twoback_avg_corr_rt = row.iloc[3]
+        twoback_avg_incorr_rt = row.iloc[4]
+        twoback_acc = row.iloc[5]
+        
+        # save pid and relavent metric to output dataframe
+        results.loc[len(results.index)] = [pid, oneback_avg_corr_rt, oneback_avg_incorr_rt, oneback_acc, twoback_avg_corr_rt, twoback_avg_incorr_rt, twoback_acc]
 
-    oneback_acc = df['oneback_corr'].dropna()
-    if (len(oneback_acc.index) < 1): oneback_acc = None
-    else: oneback_acc = oneback_acc.iloc[0]
-
-    twoback_avg_corr_rt = df['twoback_avg_corr'].dropna()
-    if (len(twoback_avg_corr_rt.index) < 1): twoback_avg_corr_rt = None
-    else: twoback_avg_corr_rt = twoback_avg_corr_rt.iloc[0]
-
-    twoback_avg_incorr_rt = df['twoback_avg_incorr'].dropna()
-    if (len(twoback_avg_incorr_rt.index) < 1): twoback_avg_incorr_rt = None
-    else: twoback_avg_incorr_rt = twoback_avg_incorr_rt.iloc[0]
-
-    twoback_acc = df['twoback_corr'].dropna()
-    if (len(twoback_acc.index) < 1): twoback_acc = None
-    else: twoback_acc = twoback_acc.iloc[0]
-
-
-    results.loc[len(results.index)] = [pid, oneback_avg_corr_rt, oneback_avg_incorr_rt, oneback_acc, twoback_avg_corr_rt, twoback_avg_incorr_rt, twoback_acc]
-
+    
+results = results.sort_values(by='PID')
+# export output dataframe to local CSV file
 results.to_csv('nback_scores.csv', sep=',', index=False)
